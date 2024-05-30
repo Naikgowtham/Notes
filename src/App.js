87b2c07eef46a3e6c82fa2a3ee937144f37a5604
Naikgowtham <naikgowtham007@gmail.com>
2024-05-30 09:48:21 +0530

    Started Notes Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import express from "express";
+import bodyParser from "body-parser";
+
+function App() {
+  return (
+    
+  );
+}
+
+export default App;
